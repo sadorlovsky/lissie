79f177864c61490fa967232343dcac780b1085bb
Zach Orlovsky <sadorlovsky@gmail.com>
2016-04-06 19:05:22 +0300

    initial commit

---
+++ src/lisa.js
@@ -0,0 +1,15 @@
+import fs from 'fs'
+
+const getAuthor = () => process.env.USER
+const getYear = () => new Date().getFullYear()
+const getLicenseText = (license) => fs.readFileSync(`../licenses/${license}`, 'utf8')
+
+const lisa = ({license='mit', author=getAuthor(), year=getYear(), email='', project=''}) => {
+  let text = getLicenseText(license)
+  text = text.replace(/{author}/, author).replace(/{year}/, year)
+  if (email !== '') text = text.replace(/{email}/, email)
+  if (project !== '') text = text.replace(/{project}/, project)
+  return text
+}
+
+export default lisa
