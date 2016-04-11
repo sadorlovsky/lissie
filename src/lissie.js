c7fe62d0451a5e192e55253448448a52b7d7c144
Zach Orlovsky <sadorlovsky@gmail.com>
2016-04-11 17:19:25 +0300

    fix path to license file

--- src/lissie.js
+++ src/lissie.js
@@ -1,8 +1,11 @@
 import fs from 'fs'
+import path from 'path'
 
 const getAuthor = () => process.env.USER
 const getYear = () => new Date().getFullYear()
-const getLicenseText = (license) => fs.readFileSync(`../licenses/${license}`, 'utf8')
+const getLicenseText = (license) => {
+  return fs.readFileSync(path.resolve(__dirname, '..', `licenses/${license}`), 'utf8')
+}
 
 const lissie = ({license='mit', author=getAuthor(), year=getYear(), email='', project=''}) => {
   let text = getLicenseText(license)
