93f6a2fea55260b7a93038dfbb928b0a9d2d175a
Zach Orlovsky <sadorlovsky@gmail.com>
2016-07-21 23:27:04 +0300

    refactor

--- src/lissie.js
+++ src/lissie.js
@@ -4,14 +4,28 @@ import path from 'path'
 const getAuthor = () => process.env.USER
 const getYear = () => new Date().getFullYear()
 const getLicenseText = license => {
-  return fs.readFileSync(path.resolve(__dirname, '..', `licenses/${license}`), 'utf8')
+  return fs.readFileSync(path.resolve('..', `licenses/${license}`), 'utf8')
+}
+const template = (text, vars) => {
+  return Object.keys(vars).filter(k => vars[k]).reduce((res, key) => {
+    const re = new RegExp(`{${key}}`)
+    return res.replace(re, vars[key])
+  }, text)
 }
 
-const lissie = ({license='mit', author=getAuthor(), year=getYear(), email='', project=''}) => {
-  let text = getLicenseText(license)
-    .replace(/{author}/, author).replace(/{year}/, year)
-  if (email !== '') { text = text.replace(/{email}/, email) }
-  if (project !== '') { text = text.replace(/{project}/, project) }
+const lissie = opts => {
+  const defaultOptions = {
+    license: 'mit',
+    author: getAuthor(),
+    year: getYear()
+  }
+  const options = Object.assign({}, defaultOptions, opts)
+  const text = template(getLicenseText(options.license), {
+    author: options.author,
+    year: options.year,
+    email: options.email,
+    project: options.project
+  })
   return text
 }
 
