bad2fd728bc7d999a0dab963a8c2a3630c3904bc
Zach Orlovsky <sadorlovsky@gmail.com>
2016-08-15 01:13:20 +0300

    refactor lissie

--- src/lissie.js
+++ src/lissie.js
@@ -4,23 +4,19 @@ import pify from 'pify'
 import assign from 'assign-defined'
 
 const getLicenseText = license => {
-  return pify(fs.readFile)(path.resolve(`licenses/${license}`), 'utf8')
+  return pify(fs.readFile)(path.resolve(__dirname, '..', 'licenses', `${license}`), 'utf8')
 }
 
 const template = (text, vars) => {
-  return Object.keys(vars).filter(k => vars[k]).reduce((res, key) => {
+  return Object.keys(assign({}, vars)).reduce((res, key) => {
     const re = new RegExp(`{${key}}`)
     return res.replace(re, vars[key])
   }, text)
 }
 
 const lissie = props => {
-  const defaultOptions = {
-    license: 'mit',
-    year: new Date().getFullYear()
-  }
-  const options = assign({}, defaultOptions, props)
-  return getLicenseText(options.license).then(text => {
+  const options = typeof props === 'string' ? { license: props } : assign({}, props)
+  return getLicenseText(options.license || 'mit').then(text => {
     return template(text, options)
   })
 }
