efbf2ee5e873b28638fecf4c35aca0f2150a6455
Zach Orlovsky <sadorlovsky@gmail.com>
2016-12-15 22:14:19 +0300

    incr coverage

--- src/cli.test.js
+++ src/cli.test.js
@@ -1,6 +1,6 @@
 import test from 'ava'
 import execa from 'execa'
-import license from './lissie'
+import license, { list } from './lissie'
 
 test('MIT without params', async t => {
   const { stdout } = await execa('src/cli.js')
@@ -10,6 +10,12 @@ test('MIT without params', async t => {
 test('MIT', async t => {
   const { stdout } = await execa('src/cli.js', ['mit'])
   t.is(stdout, await license('mit'))
+})
+
+test('list', async t => {
+  const { stdout } = await execa('src/cli.js', ['list'])
+  const licenses = await list()
+  t.is(stdout, licenses.map(l => `⚫ ${l}`).join('\n'))
 })
 
 test('Error message if license not exists', async t => {
