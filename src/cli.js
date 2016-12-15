b16704a6b6b33773614e55a316acdd49876440a8
Zach Orlovsky <sadorlovsky@gmail.com>
2016-12-15 16:33:23 +0300

    test improvments

--- src/cli.js
+++ src/cli.js
@@ -1,18 +1,38 @@
+#!/usr/bin/env node
+
 const meow = require('meow')
 const chalk = require('chalk')
 const lissie = require('./lissie')
 
 const cli = meow(`
   Usage
-    $ lissie <license>
-`)
+    $ license <license>
+
+  Options
+    --author, -a
+    --year,   -y
+
+  Examples
+    $ license
+    $ license mit
+    $ license mit -a "Zach Orlovsky"
+`, {
+  alias: {
+    a: 'author',
+    y: 'year'
+  }
+})
 
 const highlight = text => text.replace(
   /\{year\}|\{author\}|\{project\}|\{email\}/gi,
   matched => chalk.black.bgYellow(matched)
 )
 
-lissie(cli.input[0] || 'mit')
+lissie({
+  license: cli.input[0] || 'mit',
+  author: cli.flags.author,
+  year: cli.flags.year
+})
   .then(highlight)
   .then(text => console.log(text))
   .catch(({ message }) => console.log(message))
--- src/cli.test.js
+++ src/cli.test.js
@@ -3,16 +3,16 @@ import execa from 'execa'
 import lissie from './lissie'
 
 test('MIT without params', async t => {
-  const { stdout } = await execa('bin/lissie.js')
+  const { stdout } = await execa('src/cli.js')
   t.is(stdout, await lissie('mit'))
 })
 
 test('MIT', async t => {
-  const { stdout } = await execa('bin/lissie.js', ['mit'])
+  const { stdout } = await execa('src/cli.js', ['mit'])
   t.is(stdout, await lissie('mit'))
 })
 
 test('Error message if license not exists', async t => {
-  const { stdout } = await execa('bin/lissie.js', ['no-exist'])
+  const { stdout } = await execa('src/cli.js', ['no-exist'])
   t.is(stdout, 'There is not that license')
 })
