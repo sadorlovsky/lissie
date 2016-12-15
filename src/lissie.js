cf815368d96ee6c222de10905735ed9966888fa2
Zach Orlovsky <sadorlovsky@gmail.com>
2016-12-15 15:22:20 +0300

    options test

--- src/lissie.js
+++ src/lissie.js
@@ -7,6 +7,12 @@ const lissie = options => {
   const license = isString(options) ? options : get(options, 'license')
   return pify(fs)
     .readFile(path.join('licenses', license), { encoding: 'utf8' })
+    .then(text => {
+      return text.replace(
+        /\{year\}|\{author\}|\{project\}|\{email\}/gi,
+        matched => options[matched.replace(/\{|\}/gi, '')] || matched
+      )
+    })
     .catch(({ code }) => {
       if (code === 'ENOENT') {
         throw new Error('There is not that license')
