b4ffbf543e26fe74c0a1ba82c50013bc9d5ec5b3
Zach Orlovsky <sadorlovsky@gmail.com>
2016-12-17 18:47:15 +0300

    automagically insert fullname

--- src/lissie.js
+++ src/lissie.js
@@ -2,21 +2,29 @@ import { readFile, readdir } from 'fs'
 import path from 'path'
 import pify from 'pify'
 import assign from 'deep-assign'
+import fullname from 'fullname'
+import pProps from 'p-props'
 
 const normalize = text => text.trim().toLowerCase().replace(' ', '-')
 
 export const list = () => pify(readdir)('licenses')
 
 export default (license = 'mit', options = {}) => {
-  const defaults = {
-    year: new Date().getFullYear()
-  }
-  const opts = assign({}, defaults, options)
-  return pify(readFile)(
-    path.join(__dirname, '..', 'licenses', normalize(license)),
-    { encoding: 'utf8' }
-  )
-  .then(text => text.replace(
+  return pProps({
+    year: Promise.resolve(new Date().getFullYear()),
+    author: fullname()
+  })
+  .then(defaults => assign({}, defaults, options))
+  .then(opts => {
+    return pify(readFile)(
+      path.join(__dirname, '..', 'licenses', normalize(license)),
+      { encoding: 'utf8' }
+    ).then(text => ({
+      opts,
+      text
+    }))
+  })
+  .then(({ opts, text }) => text.replace(
     /\{year\}|\{author\}|\{project\}|\{email\}/gi,
     matched => opts[matched.replace(/\{|\}/gi, '')] || matched
   ))
