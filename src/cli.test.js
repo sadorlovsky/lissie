468fe1ee62049fe2bcc6e2fc165419f21fe200c9
Zach Orlovsky <sadorlovsky@gmail.com>
2016-12-22 15:16:18 +0300

    change test

--- src/cli.test.js
+++ src/cli.test.js
@@ -3,12 +3,12 @@ import execa from 'execa'
 import license, { list } from './lissie'
 
 test('MIT without params', async t => {
-  const { stdout } = await execa('src/cli.js')
+  const { stdout } = await execa('src/cli.js', ['--no-magic'])
   t.is(stdout, await license('mit'))
 })
 
 test('MIT', async t => {
-  const { stdout } = await execa('src/cli.js', ['mit'])
+  const { stdout } = await execa('src/cli.js', ['mit', '--no-magic'])
   t.is(stdout, await license('mit'))
 })
 
